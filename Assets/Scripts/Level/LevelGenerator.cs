ed5e406560169fe4376f92d45dbd14dfa89cb96d
Jack Vine <jackv949@gmail.com>
2017-06-29 20:26:02 +0930

    Level generator started
    Currently tiles just spawn on top of each other. Need actual tile placement.

---
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+	public LevelGeneratorProfile profile;
+
+	private void Start()
+	{
+		GameObject startObj = (GameObject)Instantiate(profile.startTile.gameObject, transform);
+
+		LevelTile startTile = startObj.GetComponent<LevelTile>();
+
+		foreach (Transform door in startTile.doors)
+		{
+			GenerateTile(door, profile.maxTrailLength);
+		}
+	}
+
+	private void GenerateTile(Transform connectingDoor, int trailLength)
+	{
+		LevelTile nextTile = null;
+
+		while(nextTile == null)
+		{
+			//Find a suitable tile (currently randomly selects tile)
+
+			LevelTile possibleTile = profile.GetRandomTile();
+
+			if (possibleTile == null)
+				return;
+			else
+			{
+				GameObject tileObj = (GameObject)Instantiate(possibleTile.gameObject, transform);
+				LevelTile tile = tileObj.GetComponent<LevelTile>();
+
+				Transform door = tile.GetRandomDoor();
+
+				Vector3 offset = door.position - connectingDoor.position;
+
+				tileObj.transform.position = connectingDoor.position + offset;
+
+				//TODO: Check if overlapping, attempt rotation, check again, if full rotation doesn't work choose another tile.
+
+				nextTile = tile;
+			}
+		}
+
+		trailLength--;
+
+		if (trailLength > 0)
+		{
+			foreach (Transform door in nextTile.doors)
+			{
+				GenerateTile(door, trailLength);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Level/LevelTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTile : MonoBehaviour
+{
+	public Transform[] doors;
+
+	[System.Serializable]
+	public class Requirement
+	{
+		public enum Type
+		{
+			Door,
+			Dungeon
+		}
+
+		public Type type;
+
+		public int amount = 1;
+	}
+
+	[Space()]
+	public List<Requirement> requirements = new List<Requirement>();
+
+	public Transform GetRandomDoor()
+	{
+		int index = Random.Range(0, doors.Length);
+
+		return doors[index];
+	}
+}
