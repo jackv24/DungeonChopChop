441a825e34005bbdf1936e829c908455331f41cc
HarrisonAIE <harisunnyt@gmail.com>
2017-06-29 15:58:48 +0930

    GameManager setup

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+
+	public int startSceneIndex = 1;
+
+	private int currentSceneIndex = -1;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	void Start()
+	{
+		StartCoroutine (SetupGame (startSceneIndex));
+	}
+
+	IEnumerator SetupGame(int index)
+	{
+		yield return SceneManager.LoadSceneAsync (index, LoadSceneMode.Additive);
+		currentSceneIndex = index;
+		// After first scene is loaded, do game setup
+	}
+
+	public void ChangeScene(int newSceneIndex)
+	{
+		StartCoroutine (ChangeSceneSequence (newSceneIndex));
+	}
+
+	IEnumerator ChangeSceneSequence(int index)
+	{
+		yield return SceneManager.UnloadSceneAsync (currentSceneIndex);
+
+		//stuff inbetween scenes
+
+		yield return SceneManager.LoadSceneAsync (index, LoadSceneMode.Additive);
+
+		currentSceneIndex = index;
+	}
+}
