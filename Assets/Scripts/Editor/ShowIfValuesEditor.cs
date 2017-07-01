8adb8fcface15accdf7f1ec62c9ec6ef90b5f861
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-07-01 21:12:18 +0930

    Working on movement. Added Follow, w/ Radius, w/ Interval

---
+++ Assets/Scripts/Editor/ShowIfValuesEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[CustomEditor(typeof(EnemyMove))]
+public class ShowIfValuesEditor : Editor {
+
+	private EnemyMove enemyMove;
+
+	void OnEnable()
+	{
+		enemyMove = (EnemyMove)target;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		//EditorGUILayout.BeginHorizontal();
+		if (enemyMove.movingType == TypesOfMoving.BasicFollow) 
+		{
+		} 
+		else if (enemyMove.movingType == TypesOfMoving.FollowInRadius) 
+		{
+			enemyMove.radius = EditorGUILayout.FloatField ("Detect Radius: ", enemyMove.radius); 
+		}
+		else if (enemyMove.movingType == TypesOfMoving.FollowWithIntervals) 
+		{
+			enemyMove.timeTillInterval = EditorGUILayout.FloatField ("Time till Interval in Secs: ", enemyMove.timeTillInterval); 
+			enemyMove.interval = EditorGUILayout.FloatField ("Interval wait in Secs: ", enemyMove.interval); 
+		}
+	}
+}
+	
---
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum TypesOfMoving
+{
+	BasicFollow,
+	FreeRome,
+	RandomRome,
+	FollowInRadius, 
+	FollowPattern,
+	FollowWithIntervals,
+
+};
+
+public class EnemyMove : MonoBehaviour 
+{
+	public TypesOfMoving movingType;
+
+	//basic move vars
+	public float moveSpeed;
+
+	//follow with intervals
+	[HideInInspector]
+	public float timeTillInterval;
+	[HideInInspector]
+	public float interval;
+
+	//radius move vars
+	[HideInInspector]
+	public float radius;
+
+	private GameObject player;
+
+	private float intervalCounter = 0;
+	private float timeTillIntervalCounter = 0;
+
+	void FixedUpdate()
+	{
+		if (player != null) 
+		{
+			//checks what enum is selected, do that
+			if (movingType == TypesOfMoving.BasicFollow) 
+			{
+				BasicFollow ();
+			}
+			else if (movingType == TypesOfMoving.FollowInRadius) 
+			{
+				FollowInRadius ();
+			}
+			else if (movingType == TypesOfMoving.FollowWithIntervals) 
+			{
+				FollowWithIntervals ();
+			}
+		} 
+		else 
+		{
+			//get the player if it hasnt
+			player = GameObject.FindGameObjectWithTag ("Player");
+		}
+	}
+
+	void BasicFollow()
+	{
+		//move towards the player
+		transform.position = Vector3.MoveTowards (transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+	}
+
+	void FollowInRadius()
+	{
+		//if the player is less then the radius away from the enemy, follow him
+		float distance = Vector3.Distance (transform.position, player.transform.position);
+		Debug.Log (distance);
+		if (distance < radius) 
+		{
+			BasicFollow ();
+		}
+	}
+
+	void FollowWithIntervals()
+	{
+		bool follow = true;
+		timeTillIntervalCounter++;
+		//once this float is greater then time till interval, the interval has started
+		if (timeTillIntervalCounter > (timeTillInterval * 60)) 
+		{
+			follow = false;
+			intervalCounter++;
+			//once this float is greater then the interval, the interval has ended
+			if (intervalCounter > (interval * 60)) 
+			{
+				follow = true;
+				intervalCounter = 0;
+				timeTillIntervalCounter = 0;
+			}
+		} 
+		if (follow) 
+		{
+			BasicFollow ();
+		}
+	}
+}
