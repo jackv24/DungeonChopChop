14f1c16aefb8f544dea1f9249027e77895d986e6
Jack Vine <jackv949@gmail.com>
2017-07-03 03:09:33 +0930

    Added custom model import settings

---
+++ Assets/Scripts/Misc/CustomImporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomImporter : AssetPostprocessor
+{
+	void OnPreprocessModel()
+	{
+		ModelImporter modelImporter = (ModelImporter)assetImporter;
+
+		//Prevent newly imported models from generating their own materials - these should be purposefully assigned
+		modelImporter.importMaterials = false;
+
+		//Most models will not use animation, and don't need animators by default
+		modelImporter.animationType = ModelImporterAnimationType.None;
+	}
+}
