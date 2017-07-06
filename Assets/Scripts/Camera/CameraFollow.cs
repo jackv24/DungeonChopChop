c9ec2f62238e3936cb398a1b0e36db192370f45f
Jack Vine <jackv949@gmail.com>
2017-07-06 10:47:33 +0930

    Added tile ground colliders and basic camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    [Space()]
+    public float height = 15.0f;
+    public float zOffset = -6.5f;
+
+    [Space()]
+    public float followSpeed = 10.0f;
+
+    void Start()
+    {
+        if(!target)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+
+            if (player)
+                target = player.transform;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if(target)
+        {
+            Vector3 targetPos = target.position;
+            targetPos += Vector3.up * height;
+            targetPos += Vector3.forward * zOffset;
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, followSpeed * Time.deltaTime);
+        }
+    }
+}
