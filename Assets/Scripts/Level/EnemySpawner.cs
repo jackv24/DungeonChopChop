a5235982117caa2e055994ddce9c2c3425dd3f07
Jack Vine <jackv949@gmail.com>
2017-07-28 14:43:03 +0930

    Enemy spawning and despawning added

--- Assets/Scripts/Level/EnemySpawner.cs
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -8,6 +8,8 @@ public class EnemySpawner : MonoBehaviour
 
 	public Helper.Probability[] enemies;
 
+	private List<GameObject> spawnedEnemies = new List<GameObject>();
+
 	public void Spawn()
 	{
 		foreach (Transform spawn in spawnPoints)
@@ -17,8 +19,21 @@ public class EnemySpawner : MonoBehaviour
 			if (enemyPrefab)
 			{
 				GameObject enemy = ObjectPooler.GetPooledObject(enemyPrefab);
-				enemy.transform.position = spawn.position;
+				enemy.transform.position = spawn.position + Vector3.up;
+				enemy.transform.rotation = Quaternion.identity;
+
+				spawnedEnemies.Add(enemy);
 			}
 		}
+	}
+
+	public void Despawn()
+	{
+		foreach (GameObject enemy in spawnedEnemies)
+		{
+			enemy.SetActive(false);
+		}
+
+		spawnedEnemies.Clear();
 	}
 }
