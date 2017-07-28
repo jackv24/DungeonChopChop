50d3c7a00441167d2959b3eead2a468f5acb650f
Jack Vine <jackv949@gmail.com>
2017-07-28 15:42:11 +0930

    If tile not cleared, respawn enemies that weren't cleared

--- Assets/Scripts/Level/EnemySpawner.cs
+++ Assets/Scripts/Level/EnemySpawner.cs
@@ -15,19 +15,48 @@ public class EnemySpawner : MonoBehaviour
 	public int respawnTileCount = 5;
 	private int tilesLeftUntilRespawn = 0;
 
+	private class UndefeatedEnemy
+	{
+		public UndefeatedEnemy(GameObject prefab, Transform spawnPoint)
+		{
+			this.prefab = prefab;
+			this.spawnPoint = spawnPoint;
+		}
+
+		public GameObject prefab;
+		public Transform spawnPoint;
+	}
+	private List<UndefeatedEnemy> undefeatedEnemies = new List<UndefeatedEnemy>();
+
 	public void Spawn()
 	{
 		if (tilesLeftUntilRespawn > 0)
 			return;
 
-		foreach (Transform spawn in spawnPoints)
+		if (undefeatedEnemies.Count <= 0)
 		{
-			GameObject enemyPrefab = Helper.GetRandomByProbability(enemies);
-
-			if (enemyPrefab)
+			foreach (Transform spawn in spawnPoints)
 			{
-				GameObject enemy = ObjectPooler.GetPooledObject(enemyPrefab);
-				enemy.transform.position = spawn.position + Vector3.up;
+				GameObject enemyPrefab = Helper.GetRandomByProbability(enemies);
+
+				if (enemyPrefab)
+				{
+					GameObject enemy = ObjectPooler.GetPooledObject(enemyPrefab);
+					enemy.transform.position = spawn.position + Vector3.up;
+					enemy.transform.rotation = Quaternion.identity;
+
+					spawnedEnemies.Add(enemy);
+
+					undefeatedEnemies.Add(new UndefeatedEnemy(enemyPrefab, spawn));
+				}
+			}
+		}
+		else
+		{
+			foreach(UndefeatedEnemy undefeatedEnemy in undefeatedEnemies)
+			{
+				GameObject enemy = ObjectPooler.GetPooledObject(undefeatedEnemy.prefab);
+				enemy.transform.position = undefeatedEnemy.spawnPoint.position + Vector3.up;
 				enemy.transform.rotation = Quaternion.identity;
 
 				spawnedEnemies.Add(enemy);
@@ -37,23 +66,28 @@ public class EnemySpawner : MonoBehaviour
 
 	public void Despawn()
 	{
-		bool tileClear = true;
+		List<UndefeatedEnemy> toRemove = new List<UndefeatedEnemy>();
 
-		foreach (GameObject enemy in spawnedEnemies)
+		for (int i = 0; i < undefeatedEnemies.Count; i++)
 		{
-			if (enemy.activeSelf)
-			{
-				tileClear = false;
-
-				//TODO: Save which enemies are still alive for respawn later
-				enemy.SetActive(false);
-			}
+			//Add defeated enemies to a list so that iterator is not edited
+			if (!spawnedEnemies[i].activeSelf)
+				toRemove.Add(undefeatedEnemies[i]);
 		}
 
+		//Remove defeated enemies
+		foreach (UndefeatedEnemy e in toRemove)
+			undefeatedEnemies.Remove(e);
+
+		toRemove.Clear();
+
+		foreach (GameObject enemy in spawnedEnemies)
+			enemy.SetActive(false);
+	
 		spawnedEnemies.Clear();
 
 		//If tile was just cleared, setup tile reset counter
-		if (tileClear && tilesLeftUntilRespawn <= 0)
+		if (undefeatedEnemies.Count <= 0 && tilesLeftUntilRespawn <= 0)
 		{
 			tilesLeftUntilRespawn = respawnTileCount;
 
@@ -64,6 +98,9 @@ public class EnemySpawner : MonoBehaviour
 	void MinusTilesLeft()
 	{
 		tilesLeftUntilRespawn--;
+
+		//If time to reset tile, just clear undefeated enemies
+		undefeatedEnemies.Clear();
 
 		//If no tiles are left, remove reset counter
 		if (tilesLeftUntilRespawn <= 0)
