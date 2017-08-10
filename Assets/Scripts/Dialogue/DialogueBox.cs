5758b5d93717002db69b760e076c73bb5d460887
Jack Vine <jackv949@gmail.com>
2017-08-10 10:43:31 +0930

    Added dialogue boxes

---
+++ Assets/Scripts/Dialogue/DialogueBox.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueBox : MonoBehaviour
+{
+	public AnimationClip closeAnim;
+
+	private Animator anim;
+
+	private Text textElement;
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+
+		textElement = GetComponentInChildren<Text>();
+	}
+
+	public void SetDialogue(string text)
+	{
+		textElement.text = text;
+	}
+
+	public void CloseDialogue()
+	{
+		if (closeAnim && anim)
+			StartCoroutine(CloseAnimation());
+		else
+			gameObject.SetActive(false);
+	}
+
+	IEnumerator CloseAnimation()
+	{
+		anim.Play(closeAnim.name);
+
+		yield return new WaitForSeconds(closeAnim.length);
+
+		gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/Dialogue/DialogueSpeaker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueSpeaker : MonoBehaviour
+{
+	public string[] lines = { "Default text" };
+
+	[Space()]
+	public float speakRange = 2.0f;
+	public LayerMask playerLayer;
+	public float textBoxHeight = 3.0f;
+
+	[Space()]
+	public GameObject dialogueBoxPrefab;
+
+	private DialogueBox currentBox;
+
+	//Does not need to happen every frame
+	void FixedUpdate()
+	{
+		Collider[] cols = Physics.OverlapSphere(transform.position, speakRange, playerLayer);
+
+		if(cols.Length > 0)
+		{
+			if(!currentBox)
+			{
+				GameObject obj = ObjectPooler.GetPooledObject(dialogueBoxPrefab);
+				obj.transform.position = transform.position + Vector3.up * textBoxHeight;
+
+				currentBox = obj.GetComponent<DialogueBox>();
+
+				if(currentBox)
+				{
+					currentBox.SetDialogue(lines[Random.Range(0, lines.Length)]);
+				}
+			}
+		}
+		else if(currentBox)
+		{
+			currentBox.CloseDialogue();
+
+			currentBox = null;
+		}
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.DrawWireSphere(transform.position, speakRange);
+	}
+}
