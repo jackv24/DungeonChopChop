5ab0b61f9bcaec12b6c3df0b67dc6962ff15d71f
Jack Vine <jackv949@gmail.com>
2017-08-17 23:07:07 +0930

    Chests can be opened in test levels without items manager

--- Assets/Scripts/Objects/Chest.cs
+++ Assets/Scripts/Objects/Chest.cs
@@ -24,13 +24,23 @@ public class Chest : MonoBehaviour {
         {
             if (col.collider.gameObject.layer == 14)
             {
-                if (ItemsManager.Instance.Keys > 0)
-                {
-                    animator.SetTrigger("Open");
-                    opened = true;
-                    ItemsManager.Instance.Keys -= 1;
-                }
+				if (ItemsManager.Instance)
+				{
+					if (ItemsManager.Instance.Keys > 0)
+					{
+						Open();
+						ItemsManager.Instance.Keys -= 1;
+					}
+				}
+				else //If there is no items manager, just open the chest anyway (for testing)
+					Open();
             }
         }
     }
+
+	void Open()
+	{
+		animator.SetTrigger("Open");
+		opened = true;
+	}
 }
