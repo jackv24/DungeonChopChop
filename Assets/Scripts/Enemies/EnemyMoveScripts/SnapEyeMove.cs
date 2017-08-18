0ef3017de3fccd85354b5eeda8cfd00e10f5a9bb
HarrisonAIE <harisunnyt@gmail.com>
2017-08-18 15:59:39 +0930

    Snapeye added

---
+++ Assets/Scripts/Enemies/EnemyMoveScripts/SnapEyeMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapEyeMove : EnemyMove 
+{
+
+    public float radius;
+
+	// Use this for initialization
+	void Start () 
+    {
+        if (IsInDistanceOfPlayer(radius))
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Idle"))
+            {
+                animator.SetTrigger("Triggered");
+            }
+        }
+        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Attacking"))
+        {
+            FollowPlayer();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -251,7 +251,7 @@ public class PlayerAttack : MonoBehaviour
     public void DisplaySlash()
     {
         GameObject slash = ObjectPooler.GetPooledObject(slashFX);
-        slash.transform.position = transform.position;
+        slash.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
         slash.transform.eulerAngles = transform.eulerAngles - slashFX.transform.eulerAngles;
         slash.transform.localScale = slashFX.transform.localScale;
         slash.GetComponent<Slash>().direction = transform.forward;
