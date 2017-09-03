db0d7c7656c48b796100cd6ad8f01fb6f49d25e1
Jack Vine <jackv949@gmail.com>
2017-09-03 14:35:47 +0930

    Added heat distortion

---
+++ Assets/Scripts/Effects/ScrollTexture.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+	public bool createInstance = false;
+	public bool scrollNormal = false;
+
+    private MeshRenderer rend;
+    public float xIncrease;
+    public float yIncrease;
+
+	private Vector2 offset;
+
+	void Start ()
+	{
+        rend = GetComponent<MeshRenderer>();
+	}
+	
+	void FixedUpdate ()
+	{
+		if (rend)
+		{
+			Material mat = rend.sharedMaterial;
+
+			if (createInstance)
+				mat = rend.material;
+
+			offset = new Vector2(offset.x + xIncrease * Time.deltaTime, offset.y + yIncrease * Time.deltaTime);
+
+			if (!scrollNormal)
+				mat.mainTextureOffset = offset;
+			else
+			{
+				mat.SetTextureOffset("_BumpMap", offset);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Misc/LinkState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkState : MonoBehaviour
+{
+	public bool isTarget = false;
+
+	public string identifier;
+
+	private void OnEnable()
+	{
+		if (isTarget)
+			return;
+
+		UpdateLinkedStates(true);
+	}
+
+	private void OnDisable()
+	{
+		if (isTarget)
+			return;
+
+		UpdateLinkedStates(false);
+	}
+
+	void UpdateLinkedStates(bool enabled)
+	{
+		LinkState[] foundStates = FindObjectsOfType<LinkState>();
+
+		List<LinkState> matchedStates = new List<LinkState>();
+
+		foreach(LinkState state in foundStates)
+		{
+			if (state.identifier == identifier)
+				matchedStates.Add(state);
+		}
+
+		foreach(LinkState state in matchedStates)
+		{
+			state.gameObject.SetActive(enabled);
+		}
+	}
+}
