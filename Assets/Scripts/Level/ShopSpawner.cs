86df34388cd7a8b0fa007a195af1dd7b1a3e88ff
Jack Vine <jackv949@gmail.com>
2017-09-08 12:11:20 +0930

    Added item spawning to shop stalls

--- Assets/Scripts/Level/ShopSpawner.cs
+++ Assets/Scripts/Level/ShopSpawner.cs
@@ -17,8 +17,19 @@ public class ShopSpawner : MonoBehaviour
 	public Direction direction;
 	private Vector3 OffsetDirection { get { return (direction == Direction.Right ? Vector3.right : Vector3.left); } }
 
+	[System.Serializable]
+	public class ShopStall
+	{
+		public ItemDatabase.ItemType itemType;
+
+		[Tooltip("If an item of this type and tier is not found, it will try a lower tier.")]
+		public int itemTier = 0;
+	}
+
 	[Header("Shop Values")]
-	public int shopStalls = 3;
+	public ItemDatabase itemDatabase;
+	[Space()]
+	public List<ShopStall> shopStalls = new List<ShopStall>();
 
 	void Start()
 	{
@@ -31,12 +42,29 @@ public class ShopSpawner : MonoBehaviour
 
 		if (shopPrefab)
 		{
-			for (int i = 0; i < shopStalls; i++)
+			for (int i = 0; i < shopStalls.Count; i++)
 			{
+				//Spawn stalls
 				GameObject shopObj = Instantiate(shopPrefab, transform);
 				shopObj.transform.localPosition = OffsetDirection * shopSpacing * i;
 
-				if(i < shopStalls - 1)
+				if (itemDatabase)
+				{
+					InventoryItem item = itemDatabase.GetItem(shopStalls[i].itemType, shopStalls[i].itemTier);
+
+					if(item)
+					{
+						Shop shop = shopObj.GetComponent<Shop>();
+
+						if(shop)
+						{
+							shop.SpawnItem(item);
+						}
+					}
+				}
+
+				//Spawn decorations between stalls
+				if(i < shopStalls.Count - 1)
 				{
 					GameObject decoratorObj = Instantiate(GetRandomDecorator(), transform);
 					decoratorObj.transform.localPosition = shopObj.transform.localPosition + OffsetDirection * decoratorSpacing;
