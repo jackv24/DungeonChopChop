d0668c39f1908cf7e456d3ee555bab12e6282966
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-09-10 11:32:53 +0930

    Paused menu added. For some reason doesn't work with mouse and keyboard yet

---
+++ Assets/Scripts/UI/Game/Pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+    bool paused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape) || InControl.InputManager.ActiveDevice.CommandWasPressed)
+        {
+            if (paused)
+            {
+                UnPauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+	}
+
+    void PauseGame()
+    {
+        paused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        ObjectPooler.PurgePools();
+        SceneManager.LoadScene("Game");
+    }
+}
