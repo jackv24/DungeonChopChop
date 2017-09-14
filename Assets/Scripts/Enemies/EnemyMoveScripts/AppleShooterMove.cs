9f93f974a715d3306fad8d0d62643aaf0cf7f64a
HarrisonAIE <harisunnyt@gmail.com>
2017-09-14 16:02:07 +0930

    Added appleshooter

---
+++ Assets/Scripts/Enemies/EnemyMoveScripts/AppleShooterMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleShooterMove : EnemyMove {
+
+    public float rotateSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+        Setup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsTag("Attacking"))
+        {
+            LookAtClosestPlayer(rotateSpeed);
+        }
+	}
+}
