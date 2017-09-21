6cb898b49a0996dee5c82b79dc6b3908b5c98715
Jack Vine <jackv949@gmail.com>
2017-09-21 11:38:37 +0930

    Random grass exit decals

---
+++ Assets/Scripts/Misc/EnableRandomGameObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableRandomGameObject : MonoBehaviour
+{
+	public GameObject[] gameObjects;
+
+	void Start()
+	{
+		foreach (GameObject obj in gameObjects)
+			obj.SetActive(false);
+
+		gameObjects[Random.Range(0, gameObjects.Length)].SetActive(true);
+	}
+}
