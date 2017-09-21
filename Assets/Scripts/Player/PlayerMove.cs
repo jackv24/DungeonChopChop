cfefb316c80789ac7f6ed067ab5a0742d79af57a
Jack Vine <jackv949@gmail.com>
2017-09-21 13:54:13 +0930

    Controller now works without inputmanager

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -46,7 +46,8 @@ public class PlayerMove : MonoBehaviour
 		else
 		{
 			input = new PlayerInputs();
-			input.SetupBindings();
+			input.AddKeyboardBindings();
+			input.AddControllerBindings();
 		}
 		characterController = GetComponent<CharacterController>();
 
