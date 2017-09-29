09323e5689e4b3f2f0e76672efcef928349e8010
Jack Vine <jackv949@gmail.com>
2017-09-29 10:38:04 +0930

    Fixed projectiles assigned not being saved

--- Assets/Scripts/Editor/AttackEditor.cs
+++ Assets/Scripts/Editor/AttackEditor.cs
@@ -25,27 +25,40 @@ public class AttackEditor : Editor {
         {
             if (enemyAttack.attackingType == TypesOfAttack.BasicShootIntervals)
             {
-                enemyAttack.timeTillInterval = EditorGUILayout.FloatField("Time till Interval in Secs: ", enemyAttack.timeTillInterval); 
+				//enemyAttack.timeTillInterval = EditorGUILayout.FloatField("Time till Interval in Secs: ", enemyAttack.timeTillInterval); 
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("timeTillInterval"), new GUIContent("Time till Interval in Secs: "));
             }
             else if (enemyAttack.attackingType == TypesOfAttack.ShootCircleIntervals)
             {
-                enemyAttack.timeTillInterval = EditorGUILayout.FloatField("Time till Interval in Secs: ", enemyAttack.timeTillInterval); 
-                enemyAttack.projAmount = EditorGUILayout.IntField("Amount to shoot in circle: ", enemyAttack.projAmount); 
-            }
+				//enemyAttack.timeTillInterval = EditorGUILayout.FloatField("Time till Interval in Secs: ", enemyAttack.timeTillInterval); 
+				//enemyAttack.projAmount = EditorGUILayout.IntField("Amount to shoot in circle: ", enemyAttack.projAmount);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("timeTillInterval"), new GUIContent("Time till Interval in Secs"));
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("projAmount"), new GUIContent("Amount to shoot in circle"));
+			}
             else if (enemyAttack.attackingType == TypesOfAttack.BasicShootRandIntervals)
             {
-                enemyAttack.minInterval = EditorGUILayout.FloatField("Min Interval in Secs: ", enemyAttack.minInterval); 
-                enemyAttack.maxInterval = EditorGUILayout.FloatField("Max Interval in Secs: ", enemyAttack.maxInterval);
-            }
+				//enemyAttack.minInterval = EditorGUILayout.FloatField("Min Interval in Secs: ", enemyAttack.minInterval); 
+				//enemyAttack.maxInterval = EditorGUILayout.FloatField("Max Interval in Secs: ", enemyAttack.maxInterval);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("minInterval"), new GUIContent("Min Interval in Secs"));
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("maxInterval"), new GUIContent("Max Interval in Secs"));
+			}
             else if (enemyAttack.attackingType == TypesOfAttack.ShootCircleRandIntervals)
             {
-                enemyAttack.projAmount = EditorGUILayout.IntField("Amount to shoot in circle: ", enemyAttack.projAmount); 
-                enemyAttack.minInterval = EditorGUILayout.FloatField("Min Interval in Secs: ", enemyAttack.minInterval); 
-                enemyAttack.maxInterval = EditorGUILayout.FloatField("Max Interval in Secs: ", enemyAttack.maxInterval);
-            }
-            enemyAttack.projecticle = (GameObject)EditorGUILayout.ObjectField("Projectile", enemyAttack.projecticle, typeof(GameObject), true);
-            enemyAttack.shootPosition = (GameObject)EditorGUILayout.ObjectField("Shoot Position", enemyAttack.shootPosition, typeof(GameObject), true);
-            enemyAttack.thrust = EditorGUILayout.FloatField("Thrust of projectile", enemyAttack.thrust);
-        }
+				//enemyAttack.projAmount = EditorGUILayout.IntField("Amount to shoot in circle: ", enemyAttack.projAmount); 
+				//enemyAttack.minInterval = EditorGUILayout.FloatField("Min Interval in Secs: ", enemyAttack.minInterval); 
+				//enemyAttack.maxInterval = EditorGUILayout.FloatField("Max Interval in Secs: ", enemyAttack.maxInterval);
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("projAmount"), new GUIContent("Amount to shoot in circle"));
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("minInterval"), new GUIContent("Min Interval in Secs"));
+				EditorGUILayout.PropertyField(serializedObject.FindProperty("maxInterval"), new GUIContent("Max Interval in Secs"));
+			}
+			//enemyAttack.projecticle = (GameObject)EditorGUILayout.ObjectField("Projectile", enemyAttack.projecticle, typeof(GameObject), true);
+			//enemyAttack.shootPosition = (GameObject)EditorGUILayout.ObjectField("Shoot Position", enemyAttack.shootPosition, typeof(GameObject), true);
+			//enemyAttack.thrust = EditorGUILayout.FloatField("Thrust of projectile", enemyAttack.thrust);
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("projecticle"), new GUIContent("Projectile"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("shootPosition"), new GUIContent("Shoot Position"));
+			EditorGUILayout.PropertyField(serializedObject.FindProperty("thrust"), new GUIContent("Thrust of projectile"));
+		}
+
+		serializedObject.ApplyModifiedProperties();
 	}
 }
