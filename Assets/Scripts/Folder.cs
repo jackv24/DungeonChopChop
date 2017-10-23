4e21d549d90988efed1f0740208159c60028f811
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-10-23 11:56:52 +1030

    Folder script added

---
+++ Assets/Scripts/Folder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Folder : MonoBehaviour {
+
+    public Component[] components = new MonoBehaviour[0];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
