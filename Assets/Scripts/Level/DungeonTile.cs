339c9174e75ff800a3eeababddc183cada0af5b5
Jack Vine <jackv949@gmail.com>
2017-10-30 20:49:46 +1030

    Dungeon tiles always face forward

--- Assets/Scripts/Level/DungeonTile.cs
+++ Assets/Scripts/Level/DungeonTile.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class DungeonTile : MonoBehaviour
 {
 	public GameObject replaceTile;
+	public bool resetRotation = true;
+
+	private LevelTile tile;
+
+	private void Awake()
+	{
+		tile = GetComponentInParent<LevelTile>();
+	}
 
 	public GameObject Replace()
 	{
@@ -14,6 +22,14 @@ public class DungeonTile : MonoBehaviour
 			GameObject obj = (GameObject)Instantiate(replaceTile, transform.parent);
 			obj.transform.localPosition = Vector3.zero;
 			obj.transform.localRotation = Quaternion.identity;
+
+			if (resetRotation)
+			{
+				//Reset rotation +180 to face forward
+				float angle = obj.transform.eulerAngles.y + 180;
+				//Rotate around tile origion so at to keep position correcty
+				obj.transform.RotateAround(tile.tileOrigin.position, Vector3.up, -angle);
+			}
 
 			//Remove this tile graphic
 			Destroy(gameObject);
