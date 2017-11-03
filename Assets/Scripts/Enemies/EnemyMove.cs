26c5add0ba893c7ea6ee024f75439996470ea218
HarrisonAIE <harisunnyt@gmail.com>
2017-11-03 13:13:33 +1030

    2 stones 1 bird charm fixed

--- Assets/Scripts/Enemies/EnemyMove.cs
+++ Assets/Scripts/Enemies/EnemyMove.cs
@@ -280,10 +280,13 @@ public class EnemyMove : MonoBehaviour
 		Collider[] enemies = Physics.OverlapSphere(transform.position, 500, enemyMask);
         foreach (Collider enemy in enemies)
         {
-            float dist = Vector3.Distance(transform.position, enemy.transform.position);
-            if (dist < radius)
+            if (enemy != GetComponent<Collider>())
             {
-                return enemy.transform;
+                float dist = Vector3.Distance(transform.position, enemy.transform.position);
+                if (dist < radius)
+                {
+                    return enemy.transform;
+                }
             }
         }
         return transform;
