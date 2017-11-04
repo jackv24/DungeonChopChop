7b98dc3072f228e5849169fd6c423ed095b77f86
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-11-04 19:59:08 +1030

    Spiker diamond spikes and spins

---
+++ Assets/Scripts/Enemies/EnemyMoveScripts/SpikerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikerMove : EnemyMove {
+
+    [Header("Spiker Values")]
+    public float attackRadius;
+    public float rotateSpeed = .1f;
+
+	// Use this for initialization
+	void Start () {
+        Setup();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (GetClosestPlayerRadius(attackRadius) != null)
+        {
+            animator.SetBool("Attack", true);
+            transform.Rotate(0, rotateSpeed, 0);
+        }
+        else 
+            animator.SetBool("Attack", false);
+            
+	}
+}
