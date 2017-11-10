fb7d3feffa86c3c364f04ff70af917fde3c278fa
Jack Vine <jackv949@gmail.com>
2017-11-10 14:23:35 +1030

    Fixed doors, made dungeon key and chest tiles always face forward

--- Assets/Scripts/Level/DungeonKeyTile.cs
+++ Assets/Scripts/Level/DungeonKeyTile.cs
@@ -13,6 +13,13 @@ public class DungeonKeyTile : MonoBehaviour
 	public GameObject keyReplaceTile;
 	public GameObject chestReplaceTile;
 
+	private LevelTile tile;
+
+    private void Awake()
+    {
+        tile = GetComponentInParent<LevelTile>();
+    }
+
 	public GameObject Replace(Type replaceType)
 	{
 		GameObject replaceTile = replaceType == Type.Key ? keyReplaceTile : chestReplaceTile;
@@ -23,6 +30,11 @@ public class DungeonKeyTile : MonoBehaviour
 			GameObject obj = (GameObject)Instantiate(replaceTile, transform.parent);
 			obj.transform.localPosition = Vector3.zero;
 			obj.transform.localRotation = Quaternion.identity;
+
+            //Reset rotation +180 to face forward
+            float angle = obj.transform.eulerAngles.y + 180;
+            //Rotate around tile origion so at to keep position correcty
+            obj.transform.RotateAround(tile.tileOrigin.position, Vector3.up, -angle);
 
 			//Remove this tile graphic
 			Destroy(gameObject);
