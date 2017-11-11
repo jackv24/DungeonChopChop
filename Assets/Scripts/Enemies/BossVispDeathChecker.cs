83dcdd3b0babe65a9f7f0a7458c05f6d6684eff1
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-11-11 10:51:45 +1030

    Fixed a few thangs

---
+++ Assets/Scripts/Enemies/BossVispDeathChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossVispDeathChecker : MonoBehaviour {
+
+    private GameObject child;
+
+	// Use this for initialization
+	void Start () 
+    {
+        child = transform.GetChild(0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        if (!child.gameObject.activeSelf)
+            gameObject.SetActive(false);
+	}
+}
