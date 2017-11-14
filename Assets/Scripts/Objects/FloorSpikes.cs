f4e26eb4bb532d0559e72ccc4faca9d1421eb4d4
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-11-14 10:58:18 +1030

    Spikes in and functional

---
+++ Assets/Scripts/Objects/FloorSpikes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public enum SpikeType
+{
+    Constantly,
+    OnContact
+}
+
+public class FloorSpikes : MonoBehaviour {
+
+    public SpikeType spikeType;
+
+    private Animator animator;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        animator = GetComponentInChildren<Animator>();
+	}
+
+    IEnumerator boolCooldown()
+    {
+        animator.SetBool("Trigger", true);
+        yield return new WaitForEndOfFrame();
+        animator.SetBool("Trigger", false);
+    }
+
+    void FixedUpdate()
+    {
+        if (spikeType == SpikeType.Constantly)
+            animator.SetBool("Trigger", true);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.layer == 14)
+            StartCoroutine(boolCooldown());
+    }
+}
