854f147c9a0ba83990abe106a2369e52a6605be2
HARRISOM\Harrison <harisunnyt@gmail.com>
2017-11-16 19:53:50 +1030

    Replace a spike prefab, may have broken other tiles (may need spikes replaced)

---
+++ Assets/Scripts/Objects/RandomObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjects : MonoBehaviour {
+
+    [System.Serializable]
+    public struct ObjectGroups
+    {
+        public GameObject[] objs;
+    }
+
+    public ObjectGroups[] props;
+    public float chanceOfHiding = 50;
+
+	// Use this for initialization
+	void Start () 
+    {
+        foreach (ObjectGroups groups in props)
+        {
+            int random = Random.Range(0, 101);
+
+            if (random < chanceOfHiding || chanceOfHiding == 100)
+            {
+                foreach(GameObject obj in groups.objs)
+                    obj.SetActive(false);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
